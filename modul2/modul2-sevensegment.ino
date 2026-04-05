6720a6693194b1286d2dd4f5880ee42c369e6e03
Luthfi Emillulfata <146956488+ellfataa@users.noreply.github.com>
2026-04-05 07:21:52 +0700

    AutoDisplay7Segment

--- modul2/modul2-sevensegment.ino
+++ modul2/modul2-sevensegment.ino
@@ -1,8 +1,10 @@
 #include <Arduino.h>
-// Inisiasi
+
+// Mapping pin segment pada digital display
 const int segmentPins[8] = {7, 6, 5, 11, 10, 8, 9, 4};
 // a b c d e f g dp
 
+// Memunculkan segment dari 0-F
 // urutan segmen: a b c d e f g dp
 byte digitPattern[16][8] = {
   {1,1,1,1,1,1,0,0}, // 0
@@ -28,10 +30,11 @@ void displayDigit(int num)
 {
   for(int i=0; i<8; i++)
   {
-    digitalWrite(segmentPins[i], digitPattern[num][i]);
+    digitalWrite(segmentPins[i], !digitPattern[num][i]); // Pake tanda ! karena anoda
   }
 }
 
+// Fungsi setup
 void setup()
 {
   for(int i=0; i<8; i++)
@@ -40,10 +43,10 @@ void setup()
   }
 }
 
-// Fungsi mematikan digit
+// Fungsi perulangan
 void loop()
 {
-  for(int i=0; i<16; i++)   //0 sampai F
+  for(int i=0; i<16; i++)   // 0 sampai F
   {
     displayDigit(i);
     delay(1000);
